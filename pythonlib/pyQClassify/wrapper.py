7c3af9b6a1fca51a03fba1b81ffc86f1f5a28ca6
Andrey Babak <sataloger@gmail.com>
2013-12-04 00:10:28 +0400

    Fixed missed forms of words

--- pythonlib/pyQClassify/wrapper.py
+++ pythonlib/pyQClassify/wrapper.py
@@ -3,59 +3,59 @@
 import os
 from pyQClassify.libpyQClassify import Agent, QClassifyError
 
-# Be careful - libpyQClassify.Agent is a singleton
-class ColorizerAgent(Agent):	
+class ColorizerAgent(object):   
     def __init__(self):
+        self.agent = Agent()
 
-    	# configuration file loaded
-    	self.configured = False
+        # configuration file loaded
+        self.configured = False
 
-    	# ready to mark up
-    	self.ready = False
+        # ready to mark up
+        self.ready = False
 
     def getIndexFileName(self):
-        return super(ColorizerAgent, self).getIndexFileName()
+        return self.agent.getIndexFileName()
 
     def index2file(self):
-    	if not self.configured:
-    		raise QClassifyError('Colorizer is not configured')
+        if not self.configured:
+            raise QClassifyError('Colorizer is not configured')
 
-        return super(ColorizerAgent, self).index2file()
+        return self.agent.index2file()
 
     def initMarkup(self):
-    	if not self.configured:
-    		raise QClassifyError('Colorizer is not configured')
+        if not self.configured:
+            raise QClassifyError('Colorizer is not configured')
 
-    	index_file = self.getIndexFileName()
-    	if not os.path.exists(index_file):
-    		raise OSError(2, "No such file or directory", index_file)
+        index_file = self.getIndexFileName()
+        if not os.path.exists(index_file):
+            raise OSError(2, "No such file or directory", index_file)
 
-        res = super(ColorizerAgent, self).initMarkup()
+        res = self.agent.initMarkup()
         self.ready = True
         return res
 
     reinitMarkup = initMarkup    
     
     def classifyPhrase(self, phrase):
-    	if not self.configured:
-    		raise QClassifyError('Colorizer is not configured')
+        if not self.configured:
+            raise QClassifyError('Colorizer is not configured')
 
-        return super(ColorizerAgent, self).classifyPhrase(phrase)
+        return self.agent.classifyPhrase(phrase)
     
     def loadConfig(self, path):
-    	if not os.path.exists(path):
-    		raise OSError(2, "No such file or directory", path)
+        if not os.path.exists(path):
+            raise OSError(2, "No such file or directory", path)
 
-        res = super(ColorizerAgent, self).loadConfig(path)
+        res = self.agent.loadConfig(path)
         self.configured = True
         return res
 
     def markup(self, text):
-    	if isinstance(text, unicode):
-    		text = text.encode('utf8')
-        return super(ColorizerAgent, self).markup(text)
+        if isinstance(text, unicode):
+            text = text.encode('utf8')
+        return self.agent.markup(text)
 
     def version(self):
-        return super(ColorizerAgent, self).version()
+        return self.agent.version()
 
 colorizer = ColorizerAgent()
